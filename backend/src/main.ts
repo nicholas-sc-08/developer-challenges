bb1e170ddcc50b7c1df03693b8d669da71a5ace4
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-02-08 16:31:15 -0300

    feat: Docker

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -20,6 +20,6 @@ async function bootstrap() {
   app.enableCors({ origin: true, credentials: true });
   app.use(cookieParser());
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
--- frontend/src/app/api/api.ts
+++ frontend/src/app/api/api.ts
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-export const api = axios.create({ baseURL: "http://localhost:3000", withCredentials: true });
+export const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL, withCredentials: true });
